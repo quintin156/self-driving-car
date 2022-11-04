2742c4673ff4f0e8dfc81cf620578fdc821d90c8
quintin156 <99997788+quintin156@users.noreply.github.com>
2022-11-04 02:49:12 -0400

    foward and reverse controls implemented

---
+++ main.js
@@ -0,0 +1,14 @@
+myCanvas.height = window.innerHeight;
+myCanvas.width = 200;
+
+const ctx = myCanvas.getContext("2d");
+const car = new Car(100, 100, 30, 50);
+
+animate(car);
+
+function animate() {
+  myCanvas.height = window.innerHeight
+  car.update();
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
